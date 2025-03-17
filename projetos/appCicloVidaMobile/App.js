60eb508a125ad09e1d87c6b7dbab83aa9f098bef
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-17 19:42:30 -0300

    Aula09

---
+++ projetos/appCicloVidaMobile/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MeuComponente from './components/meuComponente';
+
+export default function App() {
+  return (
+    <View>
+      <MeuComponente/>
+    </View>
+  );
+}
+
+
