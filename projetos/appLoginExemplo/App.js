5771bcf0b00ed2b8f2c75b6daff3935d499ef96a
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-17 21:39:17 -0300

    Aula09_2

---
+++ projetos/appLoginExemplo/App.js
@@ -0,0 +1,12 @@
+
+import { View } from 'react-native';
+import App2 from './components/LoginScreen';
+
+export default function App() {
+  return (
+    <View>
+      <App2/>
+    </View>
+  );
+}
+
